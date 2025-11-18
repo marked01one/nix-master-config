a172a693059f212f6f2b180f27712729013be246
marked01one <minhkhoitran2k3@gmail.com>
2025-11-18 00:37:49 -0800

    Added Wezterm lua configuration

---
+++ config/wezterm/commands/init.lua
@@ -0,0 +1,9 @@
+local toggle_transparency = require(
+  "commands.toggle-transparency"
+)
+
+local M = {
+  toggle_transparency
+}
+
+return M
---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,58 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local commands = require("commands")
+
+local config = wezterm.config_builder()
+
+config.enable_wayland = true
+
+config.initial_cols = 100
+config.initial_rows = 28
+
+config.line_height = 1.0
+
+config.color_scheme = 'Catppuccin Mocha'
+
+-- Colors
+config.colors = {
+  cursor_bg = "white",
+  cursor_border = "white"
+}
+
+-- Appearance
+config.window_decorations = "NONE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 4,
+  right = 4,
+  top = 2,
+  bottom = 2
+}
+
+-- Fonts
+config.font_size = 12
+config.font = wezterm.font_with_fallback {
+  -- 1. Primary font for alpha-numeric characters & common symbols.
+  {
+    family = "JetBrainsMono Nerd Font",
+    weight = "Regular"
+  },
+  -- 2. CJK Fallback font
+  {
+    -- Default fallback to Simplified Chinese for regional variants
+    family = "Noto Sans Mono CJK SC",
+    weight = "Regular",
+    scale = 1.05
+  }
+}
+
+-- Custom commands
+wezterm.on("augment-command-palette", function()
+  return commands
+end)
+
+-- Miscellaneous settings
+config.max_fps = 240
+config.prefer_egl = true
+
+return config
