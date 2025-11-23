b7272cc0f91161cb52647849497599db19646867
marked01one <minhkhoitran2k3@gmail.com>
2025-11-23 15:35:00 -0800

    Additional starship configurations.

--- home/starship.nix
+++ home/starship.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   programs.starship = {
@@ -10,16 +10,43 @@
     # For the full list of options, see: https://starship.rs/config/
     settings = {
       add_newline = false;
-      format = lib.concatString [
-        "$line_break"
-        "$package"
-        "$line_break"
-        "$character"
-      ];
+      format = ''
+        $username@$hostname :: $all
+        $directory$character
+      '';
       scan_timeout = 10;
       character = {
         success_symbol = "➜";
         error_symbol = "➜";
+      };
+
+      # Directory option.
+      directory = {
+        truncation_length = 2;
+        truncate_to_repo = true;
+        truncation_symbol = ".../";
+      };
+
+      # Git branch options.
+      git_branch = {
+        always_show_remote = false;
+        format = "[$symbol$branch(:$remote_branch)]($style) ";
+        style = "bold purple";
+      };
+
+      # Hostname options.
+      hostname = {
+        format = "[$ssh_symbol$hostname]($style)";
+        ssh_only = false;
+        style = "underline bold dimmed green";
+      };
+
+      # Username options.
+      username = {
+        format = "[$user]($style)";
+        show_always = true;
+        style_root = "bold red";
+        style_user = "bold yellow";
       };
     };
   };
