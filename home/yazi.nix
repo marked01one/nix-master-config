27e7c1f5cba9951b962b0dfce600177086f93d3a
marked01one <minhkhoitran2k3@gmail.com>
2025-11-23 20:09:42 -0800

    Added yazi Nix config.

---
+++ home/yazi.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+    package = pkgs.unstable.yazi.override {
+      _7zz = pkgs._7zz-rar;  # Support for RAR extraction
+    };
+
+    extraPackages = with pkgs.unstable; [
+      fd
+      ffmpeg
+      file
+      fzf
+      glow
+      imagemagick
+      jq
+      ouch
+      poppler
+      resvg
+      ripgrep
+      wl-clipboard
+      zoxide
+    ];
+  };
+}
