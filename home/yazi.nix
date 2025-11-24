970b62e7e45beeb606f912aa31cda9f6d002a158
marked01one <minhkhoitran2k3@gmail.com>
2025-11-24 10:02:16 -0800

    yazi.toml migration to Nix.

--- home/yazi.nix
+++ home/yazi.nix
@@ -2,14 +2,6 @@
 let
   dotfiles = config.lib.file.mkOutOfStoreSymlink "${cwd}/dotfiles";
 in {
-  programs.yazi = {
-    enable = true;
-    enableZshIntegration = true;
-    package = pkgs.yazi.override {
-      _7zz = pkgs._7zz-rar;  # Support for RAR extraction
-    };
-  };
-
   home.packages = with pkgs; [
     fd
     ffmpeg
@@ -27,9 +19,35 @@ in {
   ];
 
   home.file = {
-    ".config/yazi" = {
-      source = "${dotfiles}/yazi";
+    # Avoid out-of-store file installation errors by specifying only Lua files
+    # to be symlinked out-of-store.
+    ".config/yazi/init.lua" = {
+      source = "${dotfiles}/yazi/init.lua";
+    };
+    "./config/yazi/plugins" = {
+      source = "${dotfiles}/yazi/plugins";
       recursive = true;
+    };
+  };
+
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+    package = pkgs.yazi.override {
+      _7zz = pkgs._7zz-rar;  # Support for RAR extraction
+    };
+
+    # Configuration written to `$HOME/.config/yazi/yazi.toml`.
+    settings = {
+      mgr = {
+        ratio = [1 4 3];
+        sort_by = "natural";
+        sort_sensitive = true;
+        sort_reverse = false;
+        sort_dir_first = true;
+        sort_translit = true;
+        linemode = "size_and_mtime";
+      };
     };
   };
 }
