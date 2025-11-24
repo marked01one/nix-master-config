23daf39c4ff86ca4de9455ac4231c5365861f389
marked01one <minhkhoitran2k3@gmail.com>
2025-11-24 10:18:24 -0800

    fix: revert yazi away from home-manager to reduce nix store clutter.

--- home/yazi.nix
+++ home/yazi.nix
@@ -21,11 +21,8 @@ in {
   home.file = {
     # Avoid out-of-store file installation errors by specifying only Lua files
     # to be symlinked out-of-store.
-    ".config/yazi/init.lua" = {
-      source = "${dotfiles}/yazi/init.lua";
-    };
-    "./config/yazi/plugins" = {
-      source = "${dotfiles}/yazi/plugins";
+    ".config/yazi" = {
+      source = "${dotfiles}/yazi";
       recursive = true;
     };
   };
@@ -35,19 +32,6 @@ in {
     enableZshIntegration = true;
     package = pkgs.yazi.override {
       _7zz = pkgs._7zz-rar;  # Support for RAR extraction
-    };
-
-    # Configuration written to `$HOME/.config/yazi/yazi.toml`.
-    settings = {
-      mgr = {
-        ratio = [1 4 3];
-        sort_by = "natural";
-        sort_sensitive = true;
-        sort_reverse = false;
-        sort_dir_first = true;
-        sort_translit = true;
-        linemode = "size_and_mtime";
-      };
     };
   };
 }
