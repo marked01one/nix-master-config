5831c8b3be5b8e2317d67a396e4cda0328a9be1b
marked01one <minhkhoitran2k3@gmail.com>
2025-11-25 00:51:50 -0800

    vscode: home-manager configuration initialization.

---
+++ home/vscode.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+{
+  programs.vscode = {
+    enable = true;
+
+    profiles.marked01one = {
+      extensions = with pkgs.vscode-extensions; [
+        fabiospampinato.vscode-highlight
+        kdl-org.kdl-v1
+        keyring.lua
+        ms-python.debugpy
+        sumneko.lua
+        tamasfe.even-better-toml
+        zhuangtongfa.material-theme
+        ms-python.python
+        ms-python.vscode-pylance
+        ms-python.vscode-python-envs
+        shd101wyy.markdown-preview-enhanced
+        pkief.material-icon-theme
+        jnoortheen.nix-ide
+        mechatroner.rainbow-csv
+        tyriar.sort-lines
+        shardulm94.trailing-spaces
+        vscodevim.vim
+        a5huynh.vscode-ron
+        eww-yuck.yuck
+        davidanson.vscode-markdownlint
+      ];
+
+      # VS Code global snippets for `marked01one`.
+      globalSnippets = {
+        todo = {
+          body = [
+            "$LINE_COMMENT TODO: $0"
+          ];
+          description = "Insert a TODO remark.";
+          prefix = ["todo"];
+        }
+      };
+
+      # VS Code keybindings for `marked01one`.
+      keybindings = [
+        # Copy to clipboard with "Ctrl+C".
+        {
+          key = "ctrl+c";
+          command = "editor.action.clipboardCopyAction";
+          when = "textInputFocus";
+        }
+      ];
+    };
+  };
+}
