847253ca3eedfd6c3ae913d27f573a0863abb210
marked01one <minhkhoitran2k3@gmail.com>
2025-11-25 11:58:45 -0800

    prismlauncher: home-manager edits.

--- home/prismlauncher.nix
+++ home/prismlauncher.nix
@@ -3,10 +3,10 @@
 {
   home.packages = with pkgs; [
     (prismlauncher.override {
-      # Add binary required by some mod
+      # Add binary required by some mods.
       additionalPrograms = [ ffmpeg ];
 
-      # Change Java runtimes available to Prism Launcher
+      # Change Java runtimes available to Prism Launcher.
       jdks = [
         graalvmPackages.graalvm-ce
         zulu
