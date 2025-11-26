ddbf4ba6c8cd039578d2a3e9b8ae3c91c8a7cb32
marked01one <minhkhoitran2k3@gmail.com>
2025-11-25 21:40:00 -0800

    Added Nix defined shell scripts.

---
+++ system/scripts.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+
+{
+  nix-submit = pkgs.writeShellApplication {
+    name = "nix-submit";
+
+    runtimeInputs = with pkgs; [ git neovim grep ];
+
+    # Read the text of the shell script from an external file.
+    text = builtins.readFile ./../scripts/nix-submit.sh;
+  };
+
+  hello = pkgs.writeShellApplication {
+    name = "hello";
+
+    runtimeInputs = with pkgs; [ cowsay lolcat ];
+
+    text = builtins.readFile ./../scripts/hello.sh;
+  }
+}
