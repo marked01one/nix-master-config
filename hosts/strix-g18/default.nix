c9eb75a4102f30ecf3f7b6853c1b70939d4d9fbd
marked01one <minhkhoitran2k3@gmail.com>
2025-11-26 16:54:09 -0800

    docker: NixOS configuration initialization.

--- hosts/strix-g18/default.nix
+++ hosts/strix-g18/default.nix
@@ -25,22 +25,16 @@ in {
     ./../../system/zsh.nix
     ./../../system/neovim.nix
     ./../../system/nix-ld.nix
+    ./../../system/docker.nix
   ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = hostname; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
   networking.dhcpcd.setHostname = false;
+  networking.hostName = hostname;
 
   # Set your time zone.
   time.timeZone = "America/Vancouver";
