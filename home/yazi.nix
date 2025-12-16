cb214a43b72cbc2ba86d71eea79babae078acdb4
marked01one <minhkhoitran2k3@gmail.com>
2025-12-16 03:54:38 -0800

    yazi: migrate TOML configs to Nix.

--- home/yazi.nix
+++ home/yazi.nix
@@ -31,8 +31,40 @@
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
+
+    # Enable support for RAR extraction.
     package = pkgs.yazi.override {
-      _7zz = pkgs._7zz-rar;  # Support for RAR extraction
+      _7zz = pkgs._7zz-rar;
+    };
+
+    # Settings in yazi.toml
+    settings = {
+      # https://yazi-rs.github.io/docs/next/configuration/yazi#mgr
+      mgr = {
+        linemode = "size_and_mtime";
+        mouse_events = [ "click" "scroll" "touch" "move" "drag" ];
+        ratio = [ 1 4 3 ];
+        scrolloff = 100;
+        show_hidden = false;
+        show_symlink = false;
+        sort_by = "natural";
+        sort_dir_first = true;
+        sort_reverse = false;
+        sort_sensitive = true;
+        sort_translit = true;
+        title_format = "{cwd}";
+      };
+
+      # https://yazi-rs.github.io/docs/next/configuration/yazi#preview
+      preview = {
+        wrap = "yes";
+        tab_size = 2;
+        max_width = 2560;
+        max_height = 5120;
+        image_filter = "triangle";
+        image_quality = 70;
+        uberzug_scale = 1.5;
+      };
     };
   };
 }
