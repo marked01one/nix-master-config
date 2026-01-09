33c9cc70d0430103f936bde8829f29946c325a50
marked01one <minhkhoitran2k3@gmail.com>
2026-01-09 02:09:24 -0800

    strix-g18: remove unncessary comments in system config.

--- hosts/strix-g18/default.nix
+++ hosts/strix-g18/default.nix
@@ -74,25 +74,6 @@ in {
     fcp
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   system.stateVersion = "25.11";
 
   nix.settings = {
