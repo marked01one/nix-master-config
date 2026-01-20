aa20193534b15b493de76a6c4d61457036dec006
marked01one <minhkhoitran2k3@gmail.com>
2026-01-20 09:39:54 -0800

    fonts: attempt to fix the lack of emojis on gnome.

--- home/fonts.nix
+++ home/fonts.nix
@@ -9,12 +9,14 @@
         "JetBrainsMono Nerd Font"
         "Noto Sans Mono CJK SC"
       ];
+      emoji = ["Noto Color Emoji"];
     };
   };
 
   home.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
+    noto-fonts-color-emoji
     nerd-fonts.jetbrains-mono
   ];
 }
