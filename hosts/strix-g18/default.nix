94cd7d15d6e46a6413a00499bfda4d4f1b334259
marked01one <minhkhoitran2k3@gmail.com>
2026-01-22 13:17:59 -0800

    added automatic storage optimizations and garbage collection.

--- hosts/strix-g18/default.nix
+++ hosts/strix-g18/default.nix
@@ -75,6 +75,28 @@ in {
     fcp
   ];
 
+  # Periodic storage optimizations.
+  nix.optimise.automatic = true;
+  nix.optimise.dates = ["03:45"];
+
+  # Garbage collection.
+  nix.gc =
+    if !pkgs.stdenv.isDarwin
+    then {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    }
+    else {
+      automatic = true;
+      interval = {
+        Weekday = 0;
+        Hour = 0;
+        Minute = 0;
+      };
+      options = "--delete-older-than 14d";
+    };
+
   system.stateVersion = "25.11";
 
   nix.settings = {
