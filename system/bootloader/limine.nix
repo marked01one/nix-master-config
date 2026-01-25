2b7900c0fa7824139231918c212bff7c55f513d3
marked01one <minhkhoitran2k3@gmail.com>
2026-01-24 22:36:51 -0800

    limine: interface configurations.

--- system/bootloader/limine.nix
+++ system/bootloader/limine.nix
@@ -7,13 +7,26 @@
       wallpapers = [./../../assets/wallpapers/tumblr-fishbloc-001.jpg];
       wallpaperStyle = "stretched";
 
+      interface = {
+        branding = "Khoi's Digital Portal...";
+        brandingColor = 7;
+      };
+
       graphicalTerminal = {
         font = {
           spacing = 0;
           scale = "2x2";
         };
 
-        background = "00000000";
+        # Using Catpuccin Frappe color scheme.
+        palette = "303446;e78284;a6d189;e5c890;8caaee;f4b8e4;81c8be;c6d0f5";
+        brightPalette = "626880;e78284;a6d189;e5c890;8caaee;f4b8e4;81c8be;c6d0f5";
+
+        background = "99303446";
+        foreground = "c6d0f5";
+
+        margin = 10;
+        marginGradient = 4;
       };
     };
   };
