8e3ecb0e26baa463fbd1a77d4036063d5d7193cd
marked01one <minhkhoitran2k3@gmail.com>
2026-02-05 12:34:07 -0800

    zen-browser: add Zen Browser flake to home.packages inputs.

--- home/zen-browser.nix
+++ home/zen-browser.nix
@@ -1,4 +1,12 @@
-{...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    inputs.zen-browser.packages.${stdenv.hostPlatform.system}.default
+  ];
+
   programs.zen-browser = {
     enable = true;
   };
