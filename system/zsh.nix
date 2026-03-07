a2ff7044e52564389a25fb8547fc1345d95c0dc9
marked01one <minhkhoitran2k3@gmail.com>
2026-03-06 23:16:02 -0800

    system: modified zsh to print fastfetch before prompt.

--- system/zsh.nix
+++ system/zsh.nix
@@ -2,4 +2,12 @@
 {...}: {
   programs.zsh.enable = true;
   environment.pathsToLink = ["/share/zsh"];
+
+  # Shell script code called during zsh shell initialisation.
+  programs.zsh.shellInit = ''
+    if [[ $- == *i* ]]
+    then
+      fastfetch
+    fi
+  '';
 }
